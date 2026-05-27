37d86c4bf39e891148a3a5ca08d613d5ceb09e44
init-zhang <init-zhang@proton.me>
2026-05-27 13:44:43 +0100

    Add config object to board and boids

--- boids.js
+++ boids.js
@@ -7,6 +7,7 @@ const COHESION_POWER   = 0.1;
 const MAX_VELOCITY     = 5;
 const MAX_ACCELERATION = 0.1;
 const LINE_MULTIPLIER = 10;
+const TRAIL = 0.8;
 
 function distanceSquared(u, v) {
     const dx = u.x - v.x;
@@ -19,40 +20,47 @@ function randomInclusiveInt(min, max) {
 }
 
 class Boid {
-    constructor(x, y) {
+    constructor(x, y, config) {
         this.x = x;
         this.y = y;
+        this.config = config;
         const angle = Math.random() * Math.PI * 2;
-        this.vx = Math.cos(angle) * MAX_ACCELERATION;
-        this.vy = Math.sin(angle) * MAX_ACCELERATION;
+        this.vx = Math.cos(angle) * this.config.maxAcceleration;
+        this.vy = Math.sin(angle) * this.config.maxAcceleration;
         this.ax = 0;
         this.ay = 0;
         this.neighbours = [];
     }
 
-    move(width, height) {
+    move() {
+        const accMagnitude = Math.sqrt(this.ax * this.ax + this.ay * this.ay);
+        if (accMagnitude > this.config.maxAcceleration) {
+            this.ax = (this.ax / accMagnitude) * this.config.maxAcceleration;
+            this.ay = (this.ay / accMagnitude) * this.config.maxAcceleration;
+        }
+
         this.vx += this.ax;
         this.vy += this.ay;
 
         const velMagnitude = Math.sqrt(this.vx * this.vx + this.vy * this.vy);
-        if (velMagnitude > MAX_VELOCITY) {
-            this.vx = (this.vx / velMagnitude) * MAX_VELOCITY;
-            this.vy = (this.vy / velMagnitude) * MAX_VELOCITY;
+        if (velMagnitude > this.config.maxVelocity) {
+            this.vx = (this.vx / velMagnitude) * this.config.maxVelocity;
+            this.vy = (this.vy / velMagnitude) * this.config.maxVelocity;
         }
 
         this.x += this.vx;
         this.y += this.vy;
 
         if (this.x < 0) {
-            this.x += width;
-        } else if (this.x > width) {
-            this.x = width - this.x;
+            this.x += this.config.width;
+        } else if (this.x > this.config.width) {
+            this.x = this.config.width - this.x;
         }
 
         if (this.y < 0) {
-            this.y += height;
-        } else if (this.y > height) {
-            this.y = height - this.y;
+            this.y += this.config.height;
+        } else if (this.y > this.config.height) {
+            this.y = this.config.height - this.y;
         }
 
         this.ax = 0;
@@ -64,7 +72,7 @@ class Boid {
         let sumY = 0;
         let count = 0;
         for (const boid of this.neighbours) {
-            if (distanceSquared(this, boid) < SEPARATION_DISTANCE_SQUARED) {
+            if (distanceSquared(this, boid) < this.config.separationDistanceSquared) {
                 sumX += boid.x;
                 sumY += boid.y;
                 count++;
@@ -72,8 +80,8 @@ class Boid {
         }
 
         if (count === 0) return;
-        this.ax += (this.x - sumX / count) * SEPARATION_POWER;
-        this.ay += (this.y - sumY / count) * SEPARATION_POWER;
+        this.ax += (this.x - sumX / count) * this.config.separationPower;
+        this.ay += (this.y - sumY / count) * this.config.separationPower;
     }
 
     alignment() {
@@ -85,8 +93,8 @@ class Boid {
             sumY += boid.vy;
         }
 
-        this.ax += (sumX / this.neighbours.length) * ALIGNMENT_POWER;
-        this.ay += (sumY / this.neighbours.length) * ALIGNMENT_POWER;
+        this.ax += (sumX / this.neighbours.length) * this.config.alignmentPower;
+        this.ay += (sumY / this.neighbours.length) * this.config.alignmentPower;
     }
 
     cohesion() {
@@ -94,7 +102,7 @@ class Boid {
         let sumY = 0;
         let count = 0;
         for (const boid of this.neighbours) {
-            if (distanceSquared(this, boid) >= SEPARATION_DISTANCE_SQUARED) {
+            if (distanceSquared(this, boid) >= this.config.separationDistanceSquared) {
                 sumX += boid.x;
                 sumY += boid.y;
                 count++;
@@ -104,27 +112,22 @@ class Boid {
         if (count === 0) return;
         const averageX = sumX / count;
         const averageY = sumY / count;
-        this.ax += (averageX - this.x) * COHESION_POWER;
-        this.ay += (averageY - this.y) * COHESION_POWER;
+        this.ax += (averageX - this.x) * this.config.cohesionPower;
+        this.ay += (averageY - this.y) * this.config.cohesionPower;
     }
 
     update() {
         this.separation();
         this.cohesion();
         this.alignment();
-
-        const accMagnitude = Math.sqrt(this.ax * this.ax + this.ay * this.ay);
-        if (accMagnitude > MAX_ACCELERATION) {
-            this.ax = (this.ax / accMagnitude) * MAX_ACCELERATION;
-            this.ay = (this.ay / accMagnitude) * MAX_ACCELERATION;
-        }
     }
 }
 
 class Board {
-    constructor(canvasId, boidsCount) {
+    constructor(canvasId, boidsCount, config) {
         this.canvas = document.getElementById(canvasId);
         this.ctx = this.canvas.getContext("2d");
+        this.config = config;
         this.boids = [];
         this.resizeCanvas();
         this.initializeBoids(boidsCount);
@@ -133,10 +136,10 @@ class Board {
     }
 
     resizeCanvas() {
-        this.width = window.innerWidth;
-        this.height = window.innerHeight;
-        this.canvas.width = this.width;
-        this.canvas.height = this.height;
+        this.config.width = window.innerWidth;
+        this.config.height = window.innerHeight;
+        this.canvas.width = this.config.width;
+        this.canvas.height = this.config.height;
     }
 
     initializeBoids(boidsCount) {
@@ -144,8 +147,9 @@ class Board {
         for (let i = 0; i < boidsCount; i++) {
             this.boids.push(
                 new Boid(
-                    randomInclusiveInt(0, this.width),
-                    randomInclusiveInt(0, this.height)
+                    randomInclusiveInt(0, this.config.width),
+                    randomInclusiveInt(0, this.config.height),
+                    this.config
                 )
             );
         }
@@ -159,7 +163,7 @@ class Board {
             for (let j = i + 1; j < this.boids.length; j++) {
                 const boid2 = this.boids[j];
 
-                if (distanceSquared(boid1, boid2) < DISTANCE_SQUARED) {
+                if (distanceSquared(boid1, boid2) < this.config.distanceSquared) {
                     boid1.neighbours.push(boid2);
                     boid2.neighbours.push(boid1);
                 }
@@ -168,8 +172,8 @@ class Board {
     }
 
     draw() {
-        this.ctx.fillStyle = "rgba(255,255,255,0.2)";
-        this.ctx.fillRect(0, 0, this.width, this.height);
+        this.ctx.fillStyle = `rgba(255,255,255,${1-this.config.trail})`;
+        this.ctx.fillRect(0, 0, this.config.width, this.config.height);
 
         this.ctx.fillStyle = "red";
         for (const boid of this.boids) {
@@ -181,7 +185,7 @@ class Board {
         for (const boid of this.boids) {
             this.ctx.beginPath();
             this.ctx.moveTo(boid.x, boid.y);
-            this.ctx.lineTo(boid.x + boid.vx * LINE_MULTIPLIER, boid.y + boid.vy * LINE_MULTIPLIER);
+            this.ctx.lineTo(boid.x + boid.vx * this.config.lineMultiplier, boid.y + boid.vy * this.config.lineMultiplier);
             this.ctx.stroke();
         }
     }
@@ -189,16 +193,26 @@ class Board {
     update() {
         this.updateNeighbours();
         for (const boid of this.boids) boid.update();
-        for (const boid of this.boids) boid.move(this.width, this.height);
+        for (const boid of this.boids) boid.move(this.config.width, this.config.height);
         this.draw();
     }
 }
 
-const board = new Board("canvas", BOIDS);
+const defaultConfig = {
+    distanceSquared: DISTANCE_SQUARED,
+    separationDistanceSquared: SEPARATION_DISTANCE_SQUARED,
+    separationPower: SEPARATION_POWER,
+    alignmentPower: ALIGNMENT_POWER,
+    cohesionPower: COHESION_POWER,
+    maxVelocity: MAX_VELOCITY,
+    maxAcceleration: MAX_ACCELERATION,
+    lineMultiplier: LINE_MULTIPLIER,
+    trail: TRAIL
+}
+const board = new Board("canvas", BOIDS, defaultConfig);
 
 function updateLoop() {
     board.update();
-    console.log(board.boids[0]);
     requestAnimationFrame(() => updateLoop());
 }
 updateLoop();
